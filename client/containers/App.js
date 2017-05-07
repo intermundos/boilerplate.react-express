9bd75c985f8614d4d3ff36899e09ca7f3a1d4540
intermundos <junk.intermundos@gmail.com>
2017-05-07 21:45:31 +0300

    added react-md

--- client/containers/App.js
+++ client/containers/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
 import '../static/styles/styles.scss'
 import './App.scss'
-
+import React from 'react'
+import Chip from 'react-md/lib/Chips'
+import Avatar from 'react-md/lib/Avatars'
+import Button from 'react-md/lib/Buttons/Button'
+import TimePicker from 'react-md/lib/Pickers/TimePickerContainer'
+import DatePicker from 'react-md/lib/Pickers/DatePickerContainer'
 
 const App = () => {
   return (
     <div className="app">
-      <button className="button">click</button>
+
+      <Button flat label="Hello, World!" />
+
+      <TimePicker
+        id="appointment"
+        placeholder="Select an appointment time"
+        className="md-cell md-cell--bottom"
+      />
+      <DatePicker
+        id="appointment"
+        label="Select an appointment date"
+        className="md-cell"
+      />
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-import express              from "express";
-import fallback from 'connect-history-api-fallback'
-import {join, resolve}      from "path";
-import webpack              from "webpack";
-import webpackConfig        from "../configuration/webpack/webpack.dev.config";
-import webpackDevMiddleware from "webpack-dev-middleware";
-import webpackHotMiddleware from "webpack-hot-middleware";
+import { join, resolve }    from "path"
+import express              from "express"
+import fallback             from 'connect-history-api-fallback'
+import morgan               from 'morgan'
+import webpack              from "webpack"
+import webpackConfig        from "../configuration/webpack/webpack.dev.config"
+import webpackDevMiddleware from "webpack-dev-middleware"
+import webpackHotMiddleware from "webpack-hot-middleware"
 
-let server          = express()
+let server = express()
 let webpackCompiler = webpack(webpackConfig)
 
 //  History API fallback
@@ -14,18 +15,26 @@ server.use(fallback({ verbose: false }))
 
 //  Webpack dev middleware
 server.use(webpackDevMiddleware(webpackCompiler, {
-    publicPath: webpackConfig.output.publicPath,
-    stats: {
-        colors: true
-    },
-    noInfo: true
+  publicPath: webpackConfig.output.publicPath,
+  stats: {
+    colors: true
+  },
+  noInfo: true
 }))
 
 // Webpack HRM
 server.use(webpackHotMiddleware(webpackCompiler))
 
+server.use(morgan('dev'))
+
 server.get('*', (req, res) => {
   res.send(join(__dirname, 'index.html'))
 })
 
-server.listen(3000, () => console.log('Running on localhost:3000'))
+const PORT = process.env.PORT || 3000
+server.listen(PORT, (error) => {
+  if (error) {
+    throw error
+  }
+  console.log(`-- server running on localhost:${ PORT }`)
+})
