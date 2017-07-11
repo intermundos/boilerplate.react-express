dce5e186823549ce23645701b6b41f207b7c4f98
intermundos <anton.stroinsky@biscience.com>
2017-07-11 11:19:17 +0300

    Build ready

--- client/containers/App.js
+++ client/containers/App.js
@@ -3,11 +3,7 @@ import './App.scss'
 import React from 'react'
 
 const App = () => {
-    return (
-        <div className="app">
-            <span>A</span>
-        </div>
-    )
+    return <div className="app">React & Redux on Express</div>
 }
 
 export default App
