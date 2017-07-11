44d94656a976ca87125d4c021ff9a6ed4c468f7f
intermundos <anton.stroinsky@biscience.com>
2017-07-11 11:46:13 +0300

    added Bulma.io css framework and fontawesome

--- client/containers/App.js
+++ client/containers/App.js
@@ -3,7 +3,13 @@ import './App.scss'
 import React from 'react'
 
 const App = () => {
-    return <div className="app">React & Redux on Express</div>
+    return (
+        <div className="app">
+            <span className="icon">
+                <i className="fa fa-home" />
+            </span>
+        </div>
+    )
 }
 
 export default App
